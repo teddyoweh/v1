0fc4a4657452b41d0f9c643f627ce9ebd73e597c
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2022-10-30 04:21:21 -0700

    updatte

--- server.js
+++ server.js
@@ -37,6 +37,6 @@ app.use(function(req, res, next) {
 //------------ Routes ------------//
 app.use('/', require('./routes/index'));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, '0.0.0.0', console.log(`Server running on PORT ${PORT}`));
+app.listen(PORT, console.log(`Server running on PORT ${PORT}`));
